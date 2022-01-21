70decea2ab7cf8005befe8de7c33d72f1e281ba9
adam05064 <48024820+adam05064@users.noreply.github.com>
2022-01-21 20:09:08 +0100

    Add files via upload

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,28 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+long long silnia(int n)
+{
+    if (n < 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * silnia(n - 1); 
+    }
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    int n;
+
+    cout << "Podaj liczbę: ";
+    cin >> n;
+    cout << "Silnia z " << n <<" wynosi: " << silnia(n) << endl;
+  
+    return 0;
+}
